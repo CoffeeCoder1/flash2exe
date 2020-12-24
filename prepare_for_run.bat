f2cb7ee304eac55bfc2f1aeee51bafec776886a7
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2020-12-24 18:33:52 -0500

    Add Welcome Message To prepare_for_run.bat

--- prepare_for_run.bat
+++ prepare_for_run.bat
@@ -1,6 +1,7 @@
 echo off
 cls
-copy flashplayer_32_sa.exe C:\tmp\Flash\flashplayer_32_sa.exe
+copy flashplayer_32_sa.exe C:\tmp\Flash\flashplayer_32_sa.exe]
+echo Welcome to the preperation tool for flash2exe!
 set /p Pathvar=Enter name of swf file:
 copy %Pathvar% C:\tmp\Flash\tmp.swf
 %SystemRoot%\explorer.exe "c:\tmp\Flash\"
