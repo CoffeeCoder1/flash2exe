8474618553cb90a25b54f96e7ae828f5fdde1343
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2020-12-25 18:30:39 -0500

    Fixed issue where prepare_for_run.bat would fail if c:\tmp\Flash did not exist

--- prepare_for_run.bat
+++ prepare_for_run.bat
@@ -1,5 +1,6 @@
 echo off
 cls
+mkdir c:\tmp\Flash\
 copy flashplayer_32_sa.exe C:\tmp\Flash\flashplayer_32_sa.exe]
 echo Welcome to the preperation tool for flash2exe!
 set /p Pathvar=Enter name of swf file:
