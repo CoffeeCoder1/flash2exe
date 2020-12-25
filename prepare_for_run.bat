e12a3cc42e4258f3ce08d33142e5134dc3403f7d
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2020-12-25 18:39:13 -0500

    Made program a bit more universal and prepare for run clear screen more to make it appear simpler and less command line like

--- prepare_for_run.bat
+++ prepare_for_run.bat
@@ -1,8 +1,11 @@
 echo off
 cls
 mkdir c:\tmp\Flash\
-copy flashplayer_32_sa.exe C:\tmp\Flash\flashplayer_32_sa.exe
+copy flashplayer_32_sa.exe C:\tmp\Flash\flashplayer_projector.exe
+cls
 echo Welcome to the preperation tool for flash2exe!
 set /p Pathvar=Enter name of swf file:
+cls
 copy %Pathvar% C:\tmp\Flash\tmp.swf
 %SystemRoot%\explorer.exe "c:\tmp\Flash\"
+cls
