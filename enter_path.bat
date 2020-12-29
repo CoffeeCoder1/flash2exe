fcaf43c7bc1fdacd590ce04d2a9bc7a0e6afa270
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2020-12-29 16:41:30 -0500

    Fixed prepare_for_run.bat and split interface and utility into two files.
    Now prepare_for_run.bat can have a file dragged onto it in explorer or it can be pointed to a file from the command line and created enter_path.bat to allow for typing in the path to swf file.

---
+++ enter_path.bat
@@ -0,0 +1,5 @@
+cls
+echo off
+cls
+set /p Pathvar=Enter name of swf file:
+prepare_for_run.bat %Pathvar%
--- prepare_for_run.bat
+++ prepare_for_run.bat
@@ -4,10 +4,11 @@ mkdir c:\tmp\Flash\
 copy flashplayer_32_sa.exe C:\tmp\Flash\flashplayer_projector.exe
 cls
 echo Welcome to the preperation tool for flash2exe!
-set /p Pathvar=Enter name of swf file:
+echo "%~1"
+set Pathvar=%1
 cls
 copy %Pathvar% C:\tmp\Flash\tmp.swf
 %SystemRoot%\explorer.exe "c:\tmp\Flash\"
 cls
 echo Done!
-timeout 1
+timeout /t 1
