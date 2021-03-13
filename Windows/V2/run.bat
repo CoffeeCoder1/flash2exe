50b172c0f839454cb5fb278c1ddba4861b45a430
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-13 18:19:54 -0500

    Update run.bat

--- Windows/V2/run.bat
+++ Windows/V2/run.bat
@@ -1,5 +1,9 @@
 @echo off
 
+echo Setting Verbose...
+
+  set Verbose="%4"
+
 echo Creating folders...
 
   IF NOT EXIST download (
@@ -24,8 +28,10 @@ echo Setting Variables...
   set SWFFile="%1"
   set 7zipDir="%2"
   set EXEName="%3"
-  set EchoInSearch="%4"
+  
+echo Done!
+
 
 echo Running downloader...
 
-  downloader.bat "%SWFFile%" "%7zipDrv%" "%EXEName%" "%EchoInSearch%"> dllog.txt
+  downloader.bat "%SWFFile%" "%7zipDrv%" "%EXEName%" "%Verbose%"> dllog.txt
