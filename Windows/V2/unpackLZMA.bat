72efaf68650cd8e5b952e8a314436b73a0eb2c71
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-13 21:05:36 -0500

    Update unpackLZMA.bat

--- Windows/V2/unpackLZMA.bat
+++ Windows/V2/unpackLZMA.bat
@@ -5,12 +5,12 @@ echo 7zip Unpacker Utility
 
 echo Setting some variables...
 
-  set SWFFile="%1"
-  set 7zipDrv="%2"
-  set flashProjectorName="%3"
-  set lzmaName="%4"
-  set 7zipPath="%5"
-  set EXEName="%6"
+  set SWFFile=%1
+  set 7zipDrv=%2
+  set flashProjectorName=%3
+  set lzmaName=%4
+  set 7zipPath=%5
+  set EXEName=%6
   
 echo Done!
 
@@ -23,4 +23,4 @@ echo Done!
 
 echo Copying needed files...
 
-  copyfiles.bat "%SWFFile%" "%7zipDrv%" "%flashProjectorName%" "%lzmaName%" "%7zipPath%" "%EXEName%" > cplog.txt
+  copyfiles.bat %SWFFile% %7zipDrv% %flashProjectorName% %lzmaName% %7zipPath% %EXEName% > cplog.txt
