75a0dbb7552fc023bd365526fc75f9052c507295
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-14 12:09:24 -0400

    Update search.bat

--- Windows/V2/search.bat
+++ Windows/V2/search.bat
@@ -2,6 +2,8 @@
 
 echo CoffeeCoder1's Search Toolkit
 
+
+:: Set some variables to make changing things easier and the code more readable
 echo Setting some variables...
 
   :: set file to look for to 7zip
@@ -21,6 +23,8 @@ echo Done!
 
 @echo %Verbose%
 
+
+:: If searchpath is empty then set it to C:\
 echo Fixing some variables...
 
   if exist "%searchPath%" ( echo Running... ) else ( set searchPath="C:\" )
@@ -28,6 +32,7 @@ echo Fixing some variables...
 echo Done!
 
 
+:: Create variable that stores found path to 7zip
 echo Creating some variables...
 
   :: If the file is found. This variable will be set
@@ -38,6 +43,8 @@ echo Done!
 
 echo Searching for 7z.exe...
 
+
+:: See if default directory of 7zip install exists, and if it does set foundFilePath to the default 7zip install directory and skip searching for the file
 echo Does "C:/Program Files/7zip/7z.exe" Exist?
 
   if exist "C:/Program Files/7-Zip/7z.exe" (
@@ -55,6 +62,7 @@ echo Does "C:/Program Files (x86)/7zip/7z.exe" Exist?
     goto :foundfile
   ) else ( echo No )
 
+:: Mostly from stackoverflow (https://stackoverflow.com/a/19782362/). Searches for file 7z.exe in searchpath
   :: echos and saves first found path
   FOR /R "%searchPath%" %%a  in (%filename%) DO (
     IF EXIST "%%~fa" (
@@ -68,11 +76,12 @@ echo Done!
 echo File not found.
 
 
+:: Location to go to once 7z.exe is found (using goto)
 :foundfile
 
 echo Done!
 
-
+:: If file is found launch unpackLZMA.bat and if it is not then download the 7zip installers with curl so user can install 7zip.
 IF EXIST "%foundFilePath%" (
   echo The path to 7zip is set to '%foundFilePath%'
   echo Unpacking Files...
