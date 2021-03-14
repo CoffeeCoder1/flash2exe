cbf87cf7f3863697f1a50ad1ac55b7f6c07aa442
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-14 10:35:29 -0400

    Add comments to run.bat

--- Windows/V2/run.bat
+++ Windows/V2/run.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:: Read Verbose Value and if it is nothing then set it to off
 echo Setting Verbose...
 
   set Verbose="%4"
@@ -11,7 +12,7 @@ echo Setting Verbose...
 
 echo Done!
 
-
+:: Create "download" folder if it does not exist
 echo Creating folders...
 
   IF NOT EXIST download (
@@ -25,12 +26,16 @@ echo Creating folders...
 
 echo Done!
 
+
+:: Download downloader.bat from the GitHub repo using curl
 echo Downloading downloader...
 
   curl -z "downloader.bat" -o "downloader.bat" "https://raw.githubusercontent.com/CoffeeCoder1/flash2exe/main/Windows/V2/downloader.bat"
 
 echo Done!
 
+
+:: Set some variables from the paramaters passed to make the code a bit more readable
 echo Setting Variables...
 
   set SWFFile="%1"
@@ -40,6 +45,7 @@ echo Setting Variables...
 echo Done!
 
 
+:: Switch to running the downloader
 echo Running downloader...
 
   start cmd /c downloader.bat %SWFFile% %7zipDrv% %EXEName% %Verbose% > dllog.txt
