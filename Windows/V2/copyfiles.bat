879e780e13d75e14874eb2e8b67fcd3c7c8cff18
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-13 21:06:56 -0500

    Update copyfiles.bat

--- Windows/V2/copyfiles.bat
+++ Windows/V2/copyfiles.bat
@@ -2,12 +2,12 @@
 
 echo Setting some variables...
 
-  set SWFFile="%1"
-  set 7zipDrv="%2"
-  set flashProjectorName="%3"
-  set lzmaName="%4"
-  set 7zipPath="%5"
-  set EXEName="%6"
+  set SWFFile=%1
+  set 7zipDrv=%2
+  set flashProjectorName=%3
+  set lzmaName=%4
+  set 7zipPath=%5
+  set EXEName=%6
   
 echo Done!
 
@@ -57,4 +57,4 @@ echo Done!
 
 echo Creating 7zip File...
 
-  packIn7z.bat "%SWFFile%" "%7zipDrv%" "%flashProjectorName%" "%lzmaName%" "%7zipPath%" "EXEName" > packfilelog.txt
+  packIn7z.bat %SWFFile% %7zipDrv% %flashProjectorName% %lzmaName% %7zipPath% %EXEName% > packfilelog.txt
