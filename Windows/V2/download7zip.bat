d27b6e56a355de9b73f4c23158288c5aba30725a
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-14 12:47:22 -0400

    Add comments to download7zip.bat

--- Windows/V2/download7zip.bat
+++ Windows/V2/download7zip.bat
@@ -1,9 +1,12 @@
 @echo off
 
+:: CD to folder with run.bat
 cd ..\..
 
+:: Download 7zip X64 with curl
   curl -O https://www.7-zip.org/a/7z1900.exe
   
+:: Download 7zip X86 with curl
 echo Downloading 7zip X64
 
   curl -O https://www.7-zip.org/a/7z1900-x64.exe
